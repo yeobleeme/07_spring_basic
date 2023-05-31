99a817a2091aff2ddc65208fec18e243aadb9725
YE0BLEE <mauyeoblee@gmail.com>
2023-05-31 19:49:54 +0900

    20230531

--- lec03_jpa_01/src/test/java/com/lec/QueryMethodTest.java
+++ lec03_jpa_01/src/test/java/com/lec/QueryMethodTest.java
@@ -1,14 +1,15 @@
 package com.lec;
 
-import java.util.Date;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.lec.domain.Board;
@@ -44,9 +45,59 @@ public class QueryMethodTest {
 //		}
 //	}
 	
+//	@Test
+//	public void testFindByContentContaining() {
+//		List<Board> boardList = boardRepository.findByContentContaining("17");
+//		System.out.println("===> 검색 결과");
+//		for(Board board:boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrContentContaining() {
+//		List<Board> boardList = boardRepository.findByTitleContainingOrContentContaining("두", "17");
+//		System.out.println("===> 검색 결과");
+//		for(Board board:boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrderBySeqDesc() {
+//		List<Board> boardList = boardRepository.findByTitleContainingOrderBySeqDesc("17");
+//		System.out.println("===> 검색 결과");
+//		for(Board board:boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContaining() {
+//		
+//		// Pageable paging = PageRequest.of(0, 5);  // limit 0, 5;
+//		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");  // order by seq desc limit 0, 5;
+//		
+//		List<Board> boardList = boardRepository.findByTitleContaining("제목", paging);
+//		System.out.println("===> 검색 결과");
+//		for(Board board:boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
 	@Test
-	public void testFindByContentContaining() {
-		List<Board> boardList = boardRepository.findByContentContaining("17");
+	public void testFindByTitleContaining() {
+		
+		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");  // order by seq desc limit 0, 5;
+		Page<Board> pageInfo = boardRepository.findByTitleContaining("제목", paging);
+		
+		System.out.println("페이지 사이즈 = " + pageInfo.getSize());
+		System.out.println("총 페이지 수 = " + pageInfo.getTotalPages());
+		System.out.println("총 레코드 수 = " + pageInfo.getTotalElements());
+		System.out.println("다음 페이지 유무 = " + pageInfo.nextPageable());
+		
+		List<Board> boardList = pageInfo.getContent();
+		
 		System.out.println("===> 검색 결과");
 		for(Board board:boardList) {
 			System.out.println("---> " + board.toString());
