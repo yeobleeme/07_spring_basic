69af0a83e406ab0df5d2608a4d3c53585691ee1e
YEOBLEE <mauyeoblee@gmail.com>
2023-09-06 00:29:49 +0900

    Update SecurityController.java

--- lec06_security/src/main/java/com/lec/controller/SecurityController.java
+++ lec06_security/src/main/java/com/lec/controller/SecurityController.java
@@ -3,7 +3,7 @@ package com.lec.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller 
 public class SecurityController {
 
 	@GetMapping("/")
