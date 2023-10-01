46f864dc9311a289957015a0e50483e06cb058a3
YEOBLEE <mauyeoblee@gmail.com>
2023-10-01 22:59:46 +0900

    Update DynamicBoardRepository.java

--- lec03_jpa_01/src/main/java/com/lec/persistence/DynamicBoardRepository.java
+++ lec03_jpa_01/src/main/java/com/lec/persistence/DynamicBoardRepository.java
@@ -1,13 +1,1 @@
-package com.lec.persistence;
 
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-import com.lec.domain.Board;
-
-// CrudRepository & Querydsl 상속
-public interface DynamicBoardRepository extends CrudRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
-	
-	
-
-}
