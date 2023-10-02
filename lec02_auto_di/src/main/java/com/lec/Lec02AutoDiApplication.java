9c93c6b5df4550ed578fc2c5d21fa511ca5652d1
YEOBLEE <mauyeoblee@gmail.com>
2023-10-02 17:31:56 +0900

    Update Lec02AutoDiApplication.java

--- lec02_auto_di/src/main/java/com/lec/Lec02AutoDiApplication.java
+++ lec02_auto_di/src/main/java/com/lec/Lec02AutoDiApplication.java
@@ -1,13 +1,1 @@
-package com.lec;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Lec02AutoDiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Lec02AutoDiApplication.class, args);
-	}
-
-}
